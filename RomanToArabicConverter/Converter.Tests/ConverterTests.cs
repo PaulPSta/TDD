2f9604061f21ceb7409f452e63c44ac28f0de529
PaulPSta <pp.stabrawa@gmail.com>
2021-04-04 09:20:41 +0200

    Create a test case - 1.1

---
+++ RomanToArabicConverter/Converter.Tests/ConverterTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Converter.Tests
+{
+    public class ConverterTests
+    {
+        [TestCase("I", 1)]
+        [TestCase("V", 5)]
+        [TestCase("X", 10)]
+        [TestCase("L", 50)]
+        [TestCase("C", 100)]
+        [TestCase("D", 500)]
+        [TestCase("M", 1000)]
+        public void Convert_ConvertSingleNumeral_WhenInputIsValid(string numeral, int expected)
+        {
+            // arrange
+            var converter = new Converter();
+
+            // act
+            var result = converter.Convert(numeral);
+
+            // assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
