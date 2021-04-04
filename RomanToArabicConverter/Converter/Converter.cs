6d9ef555a28b8a9ec601c99b5360abc6b06daf8b
PaulPSta <pp.stabrawa@gmail.com>
2021-04-04 09:27:58 +0200

    Refactor code - 1.3

--- RomanToArabicConverter/Converter/Converter.cs
+++ RomanToArabicConverter/Converter/Converter.cs
@@ -1,22 +1,23 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Converter
 {
     public class Converter
     {
+        private readonly Dictionary<string, int> _romanToArabicMap = new Dictionary<string, int>
+        {
+            { "I", 1 },
+            { "V", 5 },
+            { "X", 10 },
+            { "L", 50 },
+            { "C", 100 },
+            { "D", 500 },
+            { "M", 1000 },
+        };
+
         public int Convert(string numeral)
         {
-            var result = numeral switch
-            {
-                "I" => 1,
-                "V" => 5,
-                "X" => 10,
-                "L" => 50,
-                "C" => 100,
-                "D" => 500,
-                "M" => 1000,
-                _ => throw new ArgumentException()
-            };
+            _romanToArabicMap.TryGetValue(numeral, out var result);
 
             return result;
         }
