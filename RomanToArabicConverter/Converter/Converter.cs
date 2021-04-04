2c5987609fd9261d09d13c88c3dc3f759c98168b
PaulPSta <pp.stabrawa@gmail.com>
2021-04-04 09:25:21 +0200

    Cover for test scenario - 1.2

---
+++ RomanToArabicConverter/Converter/Converter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Converter
+{
+    public class Converter
+    {
+        public int Convert(string numeral)
+        {
+            var result = numeral switch
+            {
+                "I" => 1,
+                "V" => 5,
+                "X" => 10,
+                "L" => 50,
+                "C" => 100,
+                "D" => 500,
+                "M" => 1000,
+                _ => throw new ArgumentException()
+            };
+
+            return result;
+        }
+    }
+}
